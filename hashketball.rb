592b47a7c075ab5326a28a38b0a785b62da970f5
Lauren Cunningham <lauren.busbee@gmail.com>
2020-08-07 16:33:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,12 +7,12 @@ def game_hash
       :number => 0,
       :shoe => 16,
       :points => 22,
-      :rebounds => 
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
-     }]
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1},
+      ]
   },
   :away =>
     {:team_name => ,
