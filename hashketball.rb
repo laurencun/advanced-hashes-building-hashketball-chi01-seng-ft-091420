9ad1cf29efc9a6a844e2d79e0ffc907c30349386
Lauren Cunningham <lauren.busbee@gmail.com>
2020-08-07 16:31:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,32 @@
-# Write your code here!
+def game_hash
+  game_hash {:home => 
+  {:team_name => "Brooklyn Nets",
+   :colors => ["Black","White"],
+   :players => [
+     {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+     }]
+  },
+  :away =>
+    {:team_name => ,
+   :colors => ,
+   :players => [
+     {:player_name =>
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+     }]
+  }
+  }
